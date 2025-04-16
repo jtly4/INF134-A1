8e34892710554a742d859d644f9f7d76d2f997cc
jtly4 <166187374+jtly4@users.noreply.github.com>
2025-04-15 22:21:21 -0700

    Update button.ts

--- src/widgets/button.ts
+++ src/widgets/button.ts
@@ -15,6 +15,7 @@ class Button extends Widget{
     private defaultFontSize: number = 18;
     private defaultWidth: number = 80;
     private defaultHeight: number = 30;
+    private clickCallback: (() => void) | null = null;
 
     constructor(parent:Window){
         super(parent);
@@ -51,13 +52,21 @@ class Button extends Widget{
     render(): void {
         this._group = (this.parent as Window).window.group();
         this._rect = this._group.rect(this.width, this.height);
-        this._rect.stroke("black");
+        this._rect.stroke("blue");
+        this._rect.fill("green");
         this._text = this._group.text(this._input);
         // Set the outer svg element 
         this.outerSvg = this._group;
         // Add a transparent rect on top of text to 
         // prevent selection cursor and to handle mouse events
         let eventrect = this._group.rect(this.width, this.height).opacity(0).attr('id', 0);
+
+        eventrect.on('click', () => {
+            if(this.clickCallback)
+            {
+                this.clickCallback();
+            }
+        });
 
         // register objects that should receive event notifications.
         // for this widget, we want to know when the group or rect objects
@@ -76,42 +85,80 @@ class Button extends Widget{
         
         super.update();
     }
-    
+
     pressReleaseState(): void{
 
         if (this.previousState instanceof PressedWidgetState)
             this.raise(new EventArgs(this));
+
+        this._rect.fill("white");
     }
+    
 
     //TODO: implement the onClick event using a callback passed as a parameter
-    onClick(/*TODO: add callback parameter*/):void{}
+    onClick(callback: () => void):void{
+        this.clickCallback = callback;
+    }
 
+    viewTime(){
+        let d = new Date();
+        return `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+    }
     
     //TODO: give the states something to do! Use these methods to control the visual appearance of your
     //widget
     idleupState(): void {
-        throw new Error("Method not implemented.");
+        if(this._group)
+        {
+            this._group.opacity(0.7);
+        }
+
+        if(this._rect)
+        {
+            this._rect.fill("gray");
+        }
+
     }
     idledownState(): void {
-        throw new Error("Method not implemented.");
+        this._group.opacity(0.3);
+        this._rect.fill("yellow");
     }
     pressedState(): void {
-        throw new Error("Method not implemented.");
+        this._rect.fill("black");
+        this._group.opacity(1);
     }
     hoverState(): void {
-        throw new Error("Method not implemented.");
+        this._rect.stroke("green");
+        this._rect.fill("red");
+        this._group.opacity(1);
     }
     hoverPressedState(): void {
-        throw new Error("Method not implemented.");
+        this._rect.stroke("red");
+        this._rect.fill("green");
     }
     pressedoutState(): void {
-        throw new Error("Method not implemented.");
+        if(this._rect)
+        { this._rect.fill("blue"); }
+        
+        this._rect.stroke("purple");
     }
     moveState(): void {
-        throw new Error("Method not implemented.");
+        this._group?.move(200,200);
+        this.positionText();
+
     }
     keyupState(keyEvent?: KeyboardEvent): void {
-        throw new Error("Method not implemented.");
+        if(keyEvent.key)
+        {
+            this._rect.stroke("orange");
+            this._rect.fill("purple");
+            this.setState(new PressedWidgetState());
+            if(this.clickCallback) {
+                this.clickCallback();
+            }
+
+            this.setState(new IdleUpWidgetState());
+        }
     }
 }
 
